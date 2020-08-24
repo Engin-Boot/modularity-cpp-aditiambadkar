521953c7f9d9668304695c408ce07e8ef13a77bd
aditiambadkar <54082235+aditiambadkar@users.noreply.github.com>
2020-08-24 18:54:06 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <assert.h>
 #include "ColorPair.h"
 
+using namespace std;
+
 void testNumberToPair(int pairNumber,
     TelCoColorCoder::MajorColor expectedMajor,
     TelCoColorCoder::MinorColor expectedMinor)
@@ -23,12 +25,23 @@ void testPairToNumber(
     assert(pairNumber == expectedPairNumber);
 }
 
+void toString() {
+    cout<<"Number to Color Mapping"<<endl;
+    cout<<"Pair Number"<<"\t"<<"Major Minor"<<endl;
+    for(int pairNumber = 1; pairNumber <= 25; pairNumber++) {
+        TelCoColorCoder::ColorPair colorPair =
+        TelCoColorCoder::GetColorFromPairNumber(pairNumber);
+        cout<<pairNumber<<"\t"<<colorPair.ToString()<<endl;
+}
+
 int main() {
     testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
     testNumberToPair(5, TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
 
     testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
     testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
+    
+    toString();
 
     return 0;
 }
